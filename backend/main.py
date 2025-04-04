ce53d73e93ae8568b82d3daccbf41c8ee452b1ff
Finn Welzmüller <finnwelzmueller97@gmail.com>
2025-04-04 19:44:54 +0200

    added sources endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv("./.env")
+mongo_password = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
+client = MongoClient(f"mongodb://admin:{mongo_password}@172.18.0.1:27017/")
+db = client["flashes"]
+sources_collection = db["sources"]
+
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Backend is running!"}
+    return {"message": "backend is running!"}
+
+@app.get("/sources/{name}")
+def get_source(name: str):
+    source = sources_collection.find_one({"_id": name})
+    if not source:
+        raise HTTPException(status_code=404, detail="Source not found")
+    return source
