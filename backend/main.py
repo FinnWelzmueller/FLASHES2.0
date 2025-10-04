2e6fe39356c298225c06b2f03a322b8db758e1c6
Finn <finnwelzmueller97@gmail.com>
2025-10-04 17:54:24 +0200

    added basic mainn script with db-connection setup and logging

--- backend/main.py
+++ backend/main.py
@@ -1,42 +1,34 @@
-from fastapi import FastAPI
-from pymongo import MongoClient
-from dotenv import load_dotenv
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.cron import CronTrigger
-from update import update
+import logging
 import os
-from datetime import datetime
-from contextlib import asynccontextmanager
-from routes import sources
+from dotenv import load_dotenv
+from pymongo import MongoClient
+from download import update
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
 
-# init
-load_dotenv("./.env")
-mongo_password = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
-client = MongoClient(f"mongodb://admin:{mongo_password}@172.18.0.1:27017/")
+load_dotenv()
+
+logging.basicConfig(
+    format="{asctime} - {levelname} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M",
+    level=getattr(logging, os.getenv("LOG_LEVEL", "INFO").upper())
+    )
+
+logging.info("Connecting to MongoDB...")
+client = MongoClient(f"mongodb://admin:{os.getenv("MONGO_INITDB_ROOT_PASSWORD")}@localhost:27017/")
 db = client['flashes']
 sources_collection = db['sources']
-scheduler = BackgroundScheduler()
+sources_collection.find({})
+logging.info("Success.")
 
-def scheduled_update():
-    print(f"[{datetime.now()}] Scheduled update triggered.")
-    update()
-
-scheduler.add_job(scheduled_update, CronTrigger(hour=2, minute=0))  # triggers at 2:00am
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    print("Starting scheduler...")
-    scheduler.start()
-    yield
-    print("Shutting down scheduler...")
-    scheduler.shutdown()
-
-
-app = FastAPI(lifespan=lifespan)
-app.include_router(sources.router)
-
-# endpoints for overview
-@app.get("/")
-def read_root():
-    return {"message": "backend is running!"}
-
+logging.info("Connecting to InfluxDB...")
+token = os.getenv("INFLUXDB_ADMIN_TOKEN")
+client = influxdb_client.InfluxDBClient(
+   url= "http://localhost:8086",
+   token=token,
+   org="flashes"
+)
+write_api = client.write_api(write_options=SYNCHRONOUS)
+logging.info("Success.")
+update(sources_collection, write_api, token, os.getenv("TEMP_DIR", "./_temp"))
