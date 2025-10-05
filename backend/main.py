f59cdc3e0ad94f743126185947de40d215d6d006
Finn <finnwelzmueller97@gmail.com>
2025-10-05 21:35:34 +0200

    started API development with fastAPI

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,7 @@ from pymongo import MongoClient
 from download import update
 import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
+from fastapi import FastAPI, Path
 
 load_dotenv()
 
@@ -16,7 +17,7 @@ logging.basicConfig(
     )
 
 logging.info("Connecting to MongoDB...")
-client = MongoClient(f"mongodb://admin:{os.getenv("MONGO_INITDB_ROOT_PASSWORD")}@localhost:27017/")
+client = MongoClient(f"mongodb://admin:{os.getenv('MONGO_INITDB_ROOT_PASSWORD')}@mongodb:27017/")
 db = client['flashes']
 sources_collection = db['sources']
 sources_collection.find({})
@@ -25,10 +26,35 @@ logging.info("Success.")
 logging.info("Connecting to InfluxDB...")
 token = os.getenv("INFLUXDB_ADMIN_TOKEN")
 client = influxdb_client.InfluxDBClient(
-   url= "http://localhost:8086",
+   url= "http://influxdb:8086",
    token=token,
    org="flashes"
 )
 write_api = client.write_api(write_options=SYNCHRONOUS)
 logging.info("Success.")
-update(sources_collection, write_api, os.getenv("TEMP_DIR", "./_temp"))
+#update(sources_collection, write_api, os.getenv("TEMP_DIR", "./_temp"))
+
+app = FastAPI()
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/sources/{source_id}")
+async def get_sources(source_id: str = Path(..., description="The ID from the mongoDB of the source to retrieve")):
+    """
+        Get a source by its ID.
+    """
+    return sources_collection.find({"_id": source_id})[0]
+
+@app.get("/health/mongo")
+async def get_health_mongo():
+    logging.info("Performing health check on MongoDB...")
+    try:
+        if (sources_collection.count_documents({})) > 0:
+            logging.info("MongoDB is healthy.")
+            return {"status": "ok"}
+        return {"status": "error"}
+    except Exception as e:
+        logging.error(f"MongoDB health check failed: {e}")
+        return {"status": "error"}
+    
