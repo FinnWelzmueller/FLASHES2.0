3179c8cdbd2b033566f4fe7318facc001be3dc8c
Finn <finnwelzmueller97@gmail.com>
2025-10-14 22:02:26 +0200

    start_scheduler now takes the parameters from update-function

--- backend/scheduler.py
+++ backend/scheduler.py
@@ -2,11 +2,11 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from download import update
 import logging
 
-def start_scheduler():
+def start_scheduler(args):
     """
     Starts the APScheduler to run the update function daily at 02:00 AM.
     """
     logging.info("Starting scheduler...")
     scheduler = BackgroundScheduler()
     logging.info("Scheduler started. Adding update job to run daily at 02:00 AM...")
-    scheduler.add_job(update, 'cron', hour=2, minute=0, second=0)
+    scheduler.add_job(update, 'cron', hour=2, minute=0, second=0, args=args)
