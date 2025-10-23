5f233b41206b70a64fd9029a8e689269ec24004c
Finn <finnwelzmueller97@gmail.com>
2025-10-23 16:58:19 +0200

    started building source-detail page. integral name is now title

--- frontend/src/app/sources/[_id]/page.tsx
+++ frontend/src/app/sources/[_id]/page.tsx
@@ -1,12 +1,22 @@
+import '@/src/app/globals.css';
+
+
 export default async function SourceDetails(
     { params }: { params: Promise<{ _id: string }> ;
 }) 
 {
     const _id = (await params)._id;
+    const res = await fetch(`http://localhost:8000/sources/${encodeURIComponent(String(_id))}`, { next: { revalidate: 0 } });
+  
+  if (!res.ok) {
+    throw new Error("The FLASHES backend appears to be down. Please try again later.");
+  }
+
+  const data = await res.json();
     return (
     <main>
-      <h1>Source Details Page</h1>
-      <p>This page displays details for a specific source {_id}.</p>
+      <h1>{data.integral_name}</h1>
+
     </main>
   );
 }
