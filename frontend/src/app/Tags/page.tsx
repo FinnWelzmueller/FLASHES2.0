2c7de6dac4f9ee6b9e5ad41702e54239ed6fdefc
Finn <finnwelzmueller97@gmail.com>
2025-10-24 15:39:01 +0200

    added content to tags-page

--- frontend/src/app/Tags/page.tsx
+++ frontend/src/app/Tags/page.tsx
@@ -1,8 +1,32 @@
-export default function Tags() {
+import { BigButton } from '@/components/bigButton';
+import { formatTag } from '@/lib/formatTag';
+import { PageDescription } from '@/components/pageDescription';
+export default async function SourcesFiltered({ params }: { params: Promise<{ tag_name: string }> ;
+}) {
+
+  const res = await fetch("http://localhost:8000/tags/", { next: { revalidate: 0 } });
+  
+  if (!res.ok) {
+    throw new Error("The FLASHES backend appears to be down. Please try again later.");
+  }
+
+  const data = await res.json();
   return (
-    <main>
-      <h1>Tags Page</h1>
-      <p>This page shows a list of all available tags to browse..</p>
+    <main className="p-6">
+      <h1>Available Tags</h1>
+      <PageDescription>
+        Choose a tag below to explore all related X-ray sources.
+      </PageDescription>
+
+      <div className="mt-10 grid grid-cols-2 md:grid-cols-4 gap-3 text-center">
+        {data.map((tag : string) => (
+
+            <BigButton
+              href={`/tags/${encodeURIComponent(tag.toLowerCase().replace(/\s+/g, '-'))}`}
+              name={`${formatTag(tag)}`}
+              key={tag}/>
+        ))}
+      </div>
     </main>
   );
 }
