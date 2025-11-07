af95585bbb0e919f113a614dd0d4f3e7b6a023f7
Finn <finnwelzmueller97@gmail.com>
2025-11-07 16:33:24 +0100

    implemented catalogue image on main page next to the welcome text

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,8 +3,10 @@ import { BigButton } from "@/components/bigButton";
 export default function Home() {
   return (
     <main>
-      <div className="space-y-4 text-neutral-300 leading-relaxed">
+      <div className="space-y-4 text-neutral-300 leading-relaxed ">
         <h1>Welcome to the FLASHES<span className="text-neutral-400">2.0</span>!</h1>
+        <div className="grid grid-cols-2 md:grid-cols-1 gap-4">
+        <div>
         <p className="space-y-4">
           This is the <b>FL</b>exible <b>A</b>lert <b>S</b>ystem for <b>H</b>igh <b>E</b>nergy <b>S</b>ources. FLASHES is a monitoring and analyses tool for x-ray sources in space. It provides lightcurve data from the <i>Monitor of All-sky X-ray image</i> (<a href="https://maxi.riken.jp/top/index.html" className="externalLink" target="_blank">MAXI</a>), the <i>Burst Array Telescope</i> (<a href="https://swift.gsfc.nasa.gov/about_swift/bat_desc.html" className="externalLink" target="_blank">BAT</a>) of Swift, and the <i>Gamma-Ray Burst Monitor</i> (<a href="https://fermi.gsfc.nasa.gov/science/instruments/gbm.html" className="externalLink" target="_blank">GBM</a>) on Fermi are automatically downloaded, processed and analysed every day. For each source in the FLASHES source catalog, a relevance is calculated. If a worth-mentioning event is happening, a high relevance value (close to 1) is assigned. If nothing happens, a low relevance value (around 0) is assigned.
         </p>
@@ -16,6 +18,15 @@ export default function Home() {
         <p className="space-y-4">
           Use the navigation bars above to explore the complete source catalogue, select tags, and learn more about FLASHES2.0. Enjoy your stay!
         </p>
+        </div>
+        <div className="w-full flex justify-center items-start">
+          <img
+            src={`http://localhost:8000/static/catalogue.png`}
+            alt="FLASHES source catalogue"
+            className="w-full h-auto rounded-2xl object-contain border dark:border-neutral-800"
+          />
+        </div>
+        </div>
       </div>
 
       <div className="space-y-8 text-neutral-300 leading-relaxed">
