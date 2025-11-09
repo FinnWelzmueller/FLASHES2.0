99fc216ccf98dccd455d11c1c4248c2fe7fd375b
Finn <finnwelzmueller97@gmail.com>
2025-11-09 21:26:24 +0100

    pagination is now in the middle below the table

--- frontend/components/sourceTable.tsx
+++ frontend/components/sourceTable.tsx
@@ -109,7 +109,7 @@ export function SourceTable<TData, TValue>({
         </TableBody>
       </Table>
           </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
+      <div className="flex items-center justify-center space-x-2 py-4">
         <Button
           variant="outline"
           size="sm"
