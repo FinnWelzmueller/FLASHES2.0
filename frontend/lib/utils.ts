77d87d83c831c2d88d7d22fe5ad2d9d43ea4233e
Finn <finnwelzmueller97@gmail.com>
2025-11-28 14:23:29 +0100

    implemented conversion-factor dictionary. Numbers from FLASHES1.0

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -4,3 +4,26 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getConversionFactor(channel: string): number {
+  /**
+   * This function returns a conversion factor to convert counts into mCrab.
+   * It is used as: mCrab = factor * counts/cm2/s
+   */
+  const conversionFactors: Record<string, number> = {
+    "2-4": 1.0/2.1 * 1000, // MAXI
+    "4-10": 1.0/1.2 * 1000, // MAXI
+    "10-20": 1.0/0.4 * 1000, // MAXI
+    "2-20": 1.0/3.2 * 1000, // MAXI
+    "15-50": 1.0/0.22 * 1000, // Swift-BAT
+    "12-50": 1.0/4.5 * 1000// Fermi-GBM
+  };
+
+  const factor = conversionFactors[channel];
+
+  if (factor === undefined) {
+    throw new Error(`No conversion factor defined for channel '${channel}'.`);
+  }
+
+  return factor;
+}
